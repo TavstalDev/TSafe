8549ad3d2ba38e7f79b70238c34e1091277e2b25
Tavstal <tavstal@gmail.com>
2024-11-22 08:50:18 +0100

    Removed UnturnedPlayer property

--- TSafe/Models/UnturnedVault.cs
+++ TSafe/Models/UnturnedVault.cs
@@ -6,16 +6,14 @@ namespace Tavstal.TSafe.Models
     public class UnturnedVault
     {
         public readonly BarricadeDrop StorageDrop;
-        public readonly UnturnedPlayer Player;
         public readonly int SizeX;
         public readonly int SizeY;
 
         public UnturnedVault() { }
         
-        public UnturnedVault(BarricadeDrop storageDrop, UnturnedPlayer player, int sizeX, int sizeY)
+        public UnturnedVault(BarricadeDrop storageDrop, int sizeX, int sizeY)
         {
             StorageDrop = storageDrop;
-            Player = player;
             SizeX = sizeX;
             SizeY = sizeY;
         }
