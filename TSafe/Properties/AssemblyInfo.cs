d1f8c770cb4d1ffc437c2f0912154b3775cc871a
Tavstal <tavstal@gmail.com>
2024-12-11 13:27:06 +0100

    Updated AssemblyInfo and TLibrary

--- TSafe/Properties/AssemblyInfo.cs
+++ TSafe/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TSafe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A RocketMod plugin for Unturned offering virtual enderchest-style storage, saved securely in an SQL database for persistent, portable access.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tavstal")]
 [assembly: AssemblyProduct("TSafe")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("2.0.*")]
